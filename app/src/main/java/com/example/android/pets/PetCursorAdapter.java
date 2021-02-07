11fe032b5270f6bd68eea9ccb3b19132c48850b7
Bishal Biswas <bishaltwentyeight@gmail.com>
2021-02-07 19:45:24 +0530

    First commit

---
+++ app/src/main/java/com/example/android/pets/PetCursorAdapter.java
@@ -0,0 +1,40 @@
+package com.example.android.pets;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.example.android.pets.data.PetContract.PetEntry;
+
+public class PetCursorAdapter extends CursorAdapter {
+
+    public PetCursorAdapter(Context context, Cursor c) {
+        super(context, c, 0);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        TextView nameTextView = (TextView)view.findViewById(R.id.name);
+        TextView summaryTextView = (TextView)view.findViewById(R.id.summary);
+
+        String petName = cursor.getString(cursor.getColumnIndexOrThrow(PetEntry.COLUMN_PET_NAME));
+        String petBreed = cursor.getString(cursor.getColumnIndexOrThrow(PetEntry.COLUMN_PET_BREED));
+
+        if (TextUtils.isEmpty(petBreed)) {
+            petBreed = context.getString(R.string.unknown_breed);
+        }
+
+        nameTextView.setText(petName);
+        summaryTextView.setText(petBreed);
+    }
+}
